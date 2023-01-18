b5bcce955b1f771163a6c7cf4c9136241f48d2aa
juangarcilazo <113950282+juangarcilazo@users.noreply.github.com>
2023-01-17 19:49:30 -0800

    api-data

--- server.js
+++ server.js
@@ -27,3 +27,38 @@ router.get('/', async (req, res) => {
     }
   });
   
+// API Function that pulls the data response [Apple STOCK]
+//fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=AAPL&apikey=demo&apikey=8CXNEA1SIH9L6U8N")
+.then(function(response){
+    return response.json();
+})
+.then(function (results) {
+    console.log(results);
+})
+.catch(function (error) {
+    console.log(Error(error));
+});
+
+//API Function that pulls data for news sentiment for [APPLE STOCK]
+//fetch("https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=8CXNEA1SIH9L6U8N")
+.then(function(response){
+    return response.json();
+})
+.then(function (results) {
+    console.log(results);
+})
+.catch(function (error) {
+    console.log(Error(error));
+});
+
+//API Function that pulls data for Company Overview for [APPLE STOCK]
+fetch("https://www.alphavantage.co/query?function=OVERVIEW&symbol=AAPL&apikey=8CXNEA1SIH9L6U8N")
+.then(function(response){
+    return response.json();
+})
+.then(function (results) {+
+    console.log(results);
+})
+.catch(function (error) {
+    console.log(Error(error));
+});
