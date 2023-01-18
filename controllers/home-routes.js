7a2fd7dfe2761713b8d48bb53128abf3d07dc645
hannahk <hanhannah839@gmail.com>
2023-01-17 21:23:56 -0800

    added login.js & logout.js, edited handlebars, edited the db name in schema.sql, and fixed syntax errror in server.js; signup&login fully functional

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -21,12 +21,13 @@ router.get('/list:listid', (req, res) => {
   });
 
 router.get('/login', (req, res) => {
-    // if (req.session.loggedIn) {
-    //   res.redirect('/');
-    //   return;
-    // }
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
   
     res.render('login');
 });
+
 
 module.exports = router;
