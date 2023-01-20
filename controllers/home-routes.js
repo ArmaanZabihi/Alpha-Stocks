ff700484890124022281918881aee4b246409763
hannahk <hanhannah839@gmail.com>
2023-01-19 23:28:07 -0800

    sign-in fully function, moved the button to nav

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,11 +2,16 @@ const router = require('express').Router();
 const { User } = require('../models');
 // const withAuth = require('../utils/auth');
 
+var handlebars = require('handlebars');
+handlebars.registerHelper("loggedIn", function(varName, varValue, options) {
+  options.data.root[varName] = varValue;
+});
+
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage'), {
+    res.render('homepage', {
       loggedIn: req.session.loggedIn,
-    }
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -23,5 +28,13 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/logout', (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('logout');
+});
 
 module.exports = router;
