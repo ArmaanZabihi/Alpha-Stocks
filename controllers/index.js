be72511391f2a7663d4028bdc7506a0b313793f1
Dillon Kim <dillonkim@Dillons-Air.attlocal.net>
2023-01-23 18:30:44 -0800

    move stocks page, show stock lists on page

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const stockRoutes = require('./stocks.js')
 
 router.use('/', homeRoutes);
+router.use('/stocks', stockRoutes)
 router.use('/api', apiRoutes);
 
 module.exports = router;
